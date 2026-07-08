0c8bf786bcd0e91f9c03b5814859fdf4737ed2b4
Nathan Call <37190397+ncall404@users.noreply.github.com>
2026-07-07 18:05:44 -0600

    Add Convert utility class for functions that do unit conversions
    First class for the new Utility namespace that will hold static classes, functions, and variables. This is done in place of the rtweekend.h utility header file that is used in the tutorial and from what I can find is the more "C# friendly" way to do this.

---
+++ src/Utility/Convert.cs
@@ -0,0 +1,8 @@
+// A static class containing useful utility functions and variables for doing unit conversions.
+
+namespace RTOneWeekend.Utility;
+
+public static class Convert
+{
+	public static double DegreesToRadians(double degrees) => degrees * Math.PI / 180;
+}
