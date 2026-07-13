3343b586d9254b0513e07ab9aa97341f0eb2c857
Nathan Call <37190397+ncall404@users.noreply.github.com>
2026-07-13 17:55:34 -0600

    Add Metal material class representing reflective surfaces

---
+++ src/Materials/Metal.cs
@@ -0,0 +1,19 @@
+// Lambertian material class for representing reflective surfaces in the scene.
+
+using RTOneWeekend.Core;
+using RTOneWeekend.Geometry;
+
+namespace RTOneWeekend.Materials;
+
+class Metal: Material
+{
+    private Vec3 albedo;
+    
+	public override bool Scatter(Ray rayIn, HitRecord rec, out Vec3 attenuation, out Ray scattered)
+	{
+		Vec3 reflected = Vec3.Reflect(rayIn.Direction, rec.Normal);
+		scattered = new Ray(rec.P, reflected);
+		attenuation = albedo;
+		return true;
+	}
+}
