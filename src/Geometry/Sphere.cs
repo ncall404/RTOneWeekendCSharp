d2f1913d950024fdbca633a66e30cd772d3c8f05
Nathan Call <37190397+ncall404@users.noreply.github.com>
2026-07-21 15:56:44 -0600

    Add bounding box to Sphere class

--- src/Geometry/Sphere.cs
+++ src/Geometry/Sphere.cs
@@ -10,12 +10,17 @@ public class Sphere : Hittable
 	private Vec3 center;
 	private double radius;
 	private Material material;
+	public override Aabb BoundingBox { get; protected set; }
 
 	public Sphere(Vec3 center, double radius, Material material)
 	{
 		this.center = center;
 		this.radius = Math.Max(0, radius);
 		this.material = material;
+
+		// Create the bounding box. NOTE: If center or radius are made dynamic in the real-time mode then send this to it's own function that is called when those are changed as well.
+		Vec3 radiusVec = new(radius, radius, radius);
+		BoundingBox = new(center - radiusVec, center + radiusVec);
 	}
 
 	public override bool Hit(in Ray r, Interval rayT, ref HitRecord rec)
