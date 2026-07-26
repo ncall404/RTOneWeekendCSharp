9d76d13b6d8006fc5d40d6b6ed59f013ecd854c3
Nathan Call <37190397+ncall404@users.noreply.github.com>
2026-07-26 16:54:54 -0600

    Create SceneManager class for tracking  and loading scenes

---
+++ src/Scenes/SceneManager.cs
@@ -0,0 +1,39 @@
+// Static class to handle scene selection and loading.
+
+namespace RTOneWeekend.Scenes;
+
+public static class SceneManager
+{
+	public static int SceneCount { get; } = 4;
+	public static int SelectedScene { get; set; } = 0;
+	public static int LoadedScene { get; set; } = 0;
+
+	public static Scene LoadScene(int id)
+	{
+		Scene activeScene;
+		switch (id)
+		{
+			case 0:
+				activeScene = new DefaultScene(); // Empty
+				break;
+			case 1:
+				activeScene = new Scene1(); // Three material spheres
+				break;
+			case 2:
+				activeScene = new Scene2(); // Bouncing spheres
+				break;
+			case 3:
+				activeScene = new Scene3(); // Checkered spheres
+				break;
+			case 4:
+				activeScene = new Scene4(); // Earth
+				break;
+			default:
+				activeScene = new DefaultScene(); // Empty
+				break;
+		}
+
+		LoadedScene = id;
+		return activeScene;
+	}
+}
