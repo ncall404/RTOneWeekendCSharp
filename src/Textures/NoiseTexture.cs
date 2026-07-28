6c035423850c9b428d5618a6b0945968061f430c
Nathan Call <37190397+ncall404@users.noreply.github.com>
2026-07-27 18:33:42 -0600

    Update NoiseTexture to allow for turbulence

--- src/Textures/NoiseTexture.cs
+++ src/Textures/NoiseTexture.cs
@@ -7,22 +7,21 @@ namespace RTOneWeekend.Textures;
 public class NoiseTexture : Texture
 {
 	private readonly Perlin _noise;
-	private double _scale;
-
-	public NoiseTexture()
-	{
-		_noise = new Perlin();
-		_scale = 1;
-	}
-
-	public NoiseTexture(double scale)
+	private readonly double _scale;
+	private readonly int _turbulance;
+ 
+	public NoiseTexture(double scale = 1, int turbulance = 0)
 	{
 		_noise = new Perlin();
 		_scale = scale;
+		_turbulance = turbulance;
 	}
 
 	public override Vec3 Value(double u, double v, Vec3 p)
 	{
+		if (_turbulance > 0)
+			return new Vec3(1, 1, 1) * _noise.Turbulance(p, _turbulance);
+
 		return new Vec3(1, 1, 1) * 0.5 * (1 + _noise.Noise(p * _scale)); // Maps values to a range of 0 to 1 to keep from negative numbers being passed to LinearToGamma()
 	}
 }
